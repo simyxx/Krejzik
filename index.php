ef5ffc8ef83fc00623f10370e0cfb7d37427f033
simyxx <simonecek2005@gmail.com>
2023-10-29 13:38:16 +0100

    Fixed timeline post

--- index.php
+++ index.php
@@ -16,6 +16,58 @@ $userData = $user->getData($_SESSION['krejzik_userid']);
 
 $USER = $userData; // Přihlášený uživatel a jeho data
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $post = new Post();
+    $id = $_SESSION['krejzik_userid'];
+
+    // Zkontrolovat, zda byl nahrán soubor
+    if (isset($_FILES['file']) && $_FILES['file']['name'] != "") {
+        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        $maxFileSize = 5 * 1024 * 1024; // 5 MB
+
+        // Získání informací o nahrávaném souboru
+        $uploadedFile = $_FILES['file'];
+        $fileName = $uploadedFile['name'];
+        $fileSize = $uploadedFile['size'];
+        $fileTmpName = $uploadedFile['tmp_name'];
+        $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+        // Zkontrolovat povolené typy souborů a maximální velikost
+        if (in_array($fileExtension, $allowedExtensions) && $fileSize <= $maxFileSize) {
+            $result = $post->create_post($id, $_POST, $_FILES);
+
+            if ($result == "") {
+                // Přesměrování pokud vše proběhlo v pořádku
+                header("Location: index.php");
+                die();
+            } else {
+                echo "<div  style='text-align:center;font-size:18px;color:white;background-color:#F16529;'>";
+                echo "Nastala chyba: <br>";
+                echo $result;
+                echo "</div>";
+            }
+        } else {
+            echo "<div  style='text-align:center;font-size:18px;color:white;background-color:#F16529;'>";
+            echo "Špatný typ souboru (povolené: jpg, jpeg, png, gif) nebo překročená maximální povolená velikost (5 MB).";
+            echo "</div>";
+        }
+    } else {
+
+        // Pokud uživatel neposkytl soubor, můžete zde provést jinou akci nebo zpracování textového příspěvku
+        $result = $post->create_post($id, $_POST, $_FILES);
+
+        if ($result == "") {
+            header("Location: index.php");
+            die();
+        } else {
+            echo "<div  style='text-align:center;font-size:18px;color:white;background-color:#F16529;'>";
+            echo "Nastala chyba: <br>";
+            echo $result;
+            echo "</div>";
+        }
+    }
+}
+
 // Získání postů
 $post = new Post();
 $id = $_SESSION['krejzik_userid'];
@@ -46,10 +98,12 @@ $posts = $post->get_posts($id);
             <div class="posts-area">
 
             <div class="new-feed">
-                    <form action="" method="POST">
-                    <textarea name="post"placeholder="Co máte na mysli?"></textarea>
-                    <button type="submit">PŘIDAT</button>
-                </form>
+            <form action="#" method="POST" enctype="multipart/form-data">
+                            <textarea name="post" placeholder="Co máte na mysli?"
+                                style="word-wrap: break-word;"></textarea>
+                            <input type="file" name="file">
+                            <button style="margin-top:20px;" type="submit">PŘIDAT</button>
+                        </form>
                 </div>
 
                 <div class="post-bar">
