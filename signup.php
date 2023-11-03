30df56f062e5faeb1bf73bc2500fcdc9b05ea964
Martin <146062823+Martinosmera@users.noreply.github.com>
2023-11-03 18:21:49 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Krátký popis obsahu stránky">
+    <meta name="keywords" content="klíčová slova, oddělená čárkami">
     <title>Krejzik | Registrace</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
